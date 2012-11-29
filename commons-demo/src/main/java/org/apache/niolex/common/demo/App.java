5f92b2ff876c7d3fb36647c8bdd94e059da72eee
Jiyun Xie <pftxie@qq.com>
2012-11-29 17:44:41 +0800

    nothing

--- commons-demo/src/main/java/org/apache/niolex/common/demo/App.java
+++ commons-demo/src/main/java/org/apache/niolex/common/demo/App.java
@@ -4,10 +4,16 @@ package org.apache.niolex.common.demo;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+    	String fileName = System.getProperty("config.client.property.file");
+		if (fileName != null) {
+			System.out.println(fileName);
+		} else {
+			System.out.println("N");
+		}
         System.out.println( "Hello World!" );
     }
 }
