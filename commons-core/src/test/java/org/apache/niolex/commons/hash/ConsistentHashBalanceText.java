52ab6486971de695d2611f43518c4e4a88713dd9
Lex <lex@java>
2013-06-05 13:07:08 +0800

    test the percentage of add and remove node at runtime for Consistent Hash

--- commons-core/src/test/java/org/apache/niolex/commons/hash/ConsistentHashBalanceText.java
+++ commons-core/src/test/java/org/apache/niolex/commons/hash/ConsistentHashBalanceText.java
@@ -17,8 +17,10 @@
  */
 package org.apache.niolex.commons.hash;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.niolex.commons.bean.Pair;
 import org.apache.niolex.commons.test.Counter;
@@ -183,4 +185,60 @@ public class ConsistentHashBalanceText {
         System.out.println("Balances500 => " + map);
     }
 
+
+    @Test
+    public void testAddBalance() throws Exception {
+        Collection<String> nodeList = Arrays.asList("10.214.133.100:8087", "10.214.133.101:8088", "10.214.65.11:8087", "10.214.65.12:8088",
+                "10.214.133.102:8087", "10.214.133.103:8088", "10.214.65.13:8087", "10.214.65.14:8088");
+        ConsistentHash<String> dHash = new ConsistentHash<String>(ConsistentHash.GuavaHash.INSTANCE, 100, nodeList);
+        Map<String, Integer> map = Maps.newHashMap();
+        Map<String, String> trap = Maps.newHashMap();
+        for (int i = 0; i < 20000; ++i) {
+            String key = MockUtil.randString();
+            String node = dHash.getNode(key);
+            Integer in = map.get(node);
+            in = in == null ? 1 : in + 1;
+            map.put(node, in);
+            trap.put(key, node);
+        }
+        Pair<Integer,Double> pair = Counter.calcMeanSquareError(map.values());
+        System.out.println("BalanceA => " + pair.toString("avg", "MSE") + ", PCT=" + (int)(pair.b / pair.a * 100) + "%");
+        dHash.add("10.13.65.131:8088");
+        int cnt = 0;
+        for (Entry<String, String> o : trap.entrySet()) {
+            String node = dHash.getNode(o.getKey());
+            if (!node.equals(o.getValue())) {
+                ++cnt;
+            }
+        }
+        System.out.println("ChangedA => " + cnt + ", PCT=" + (cnt * 100 / 20000) + "%");
+    }
+
+    @Test
+    public void testRemoveBalance() throws Exception {
+        Collection<String> nodeList = Arrays.asList("10.214.133.100:8087", "10.214.133.101:8088", "10.214.65.11:8087", "10.214.65.12:8088",
+                "10.214.133.102:8087", "10.214.133.103:8088", "10.214.65.13:8087", "10.214.65.14:8088");
+        ConsistentHash<String> dHash = new ConsistentHash<String>(ConsistentHash.GuavaHash.INSTANCE, 100, nodeList);
+        Map<String, String> trap = Maps.newHashMap();
+        for (int i = 0; i < 20000; ++i) {
+            String key = MockUtil.randString();
+            String node = dHash.getNode(key);
+            trap.put(key, node);
+        }
+        dHash.remove("10.214.133.101:8088");
+        int cnt = 0;
+        Map<String, Integer> map = Maps.newHashMap();
+        for (Entry<String, String> o : trap.entrySet()) {
+            String node = dHash.getNode(o.getKey());
+            Integer in = map.get(node);
+            in = in == null ? 1 : in + 1;
+            map.put(node, in);
+            if (!node.equals(o.getValue())) {
+                ++cnt;
+            }
+        }
+        Pair<Integer,Double> pair = Counter.calcMeanSquareError(map.values());
+        System.out.println("BalanceR => " + pair.toString("avg", "MSE") + ", PCT=" + (int)(pair.b / pair.a * 100) + "%");
+        System.out.println("ChangedR => " + cnt + ", PCT=" + (cnt * 100 / 20000) + "%");
+    }
 }
