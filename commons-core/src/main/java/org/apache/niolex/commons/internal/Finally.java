af20ba854071d739dcc1d630e51629cb7ce7e902
Jiyun Xie <xiejiyun@gmail.com>
2013-07-27 14:05:33 +0800

    add read write lock support in finally

--- commons-core/src/main/java/org/apache/niolex/commons/internal/Finally.java
+++ commons-core/src/main/java/org/apache/niolex/commons/internal/Finally.java
@@ -20,6 +20,8 @@ package org.apache.niolex.commons.internal;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author <a href="mailto:xiejiyun@foxmail.com">Xie, Jiyun</a>
@@ -28,6 +30,13 @@ import java.io.OutputStream;
  */
 public class Finally {
 
+    /**
+     * Write the data to the output stream and close it.
+     *
+     * @param zout
+     * @param data
+     * @throws IOException
+     */
     public static void writeAndClose(OutputStream zout, byte[] data) throws IOException {
         try {
             zout.write(data);
@@ -36,6 +45,14 @@ public class Finally {
         }
     }
 
+    /**
+     * Transfer all the data from the input stream to the output stream and close them.
+     *
+     * @param in
+     * @param out
+     * @param BUF_SIZE
+     * @throws IOException
+     */
     public static void transferAndClose(InputStream in, OutputStream out, final int BUF_SIZE) throws IOException {
         try {
             byte[] data = new byte[BUF_SIZE];
@@ -46,6 +63,46 @@ public class Finally {
         } finally {
             in.close();
             out.close();
+        }
+    }
+
+    /**
+     * Use read lock to invoke this method.
+     *
+     * @param lock
+     * @param proxy
+     * @param method
+     * @param args
+     * @return the result
+     * @throws Throwable
+     */
+    public static Object useReadLock(ReentrantReadWriteLock lock, Object proxy, Method method, Object[] args)
+            throws Throwable {
+        lock.readLock().lock();
+        try {
+            return method.invoke(proxy, args);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    /**
+     * Use write lock to invoke this method.
+     *
+     * @param lock
+     * @param proxy
+     * @param method
+     * @param args
+     * @return the result
+     * @throws Throwable
+     */
+    public static Object useWriteLock(ReentrantReadWriteLock lock, Object proxy, Method method, Object[] args)
+            throws Throwable {
+        lock.writeLock().lock();
+        try {
+            return method.invoke(proxy, args);
+        } finally {
+            lock.writeLock().unlock();
         }
     }
 
--- commons-core/src/test/java/org/apache/niolex/commons/internal/FinallyTest.java
+++ commons-core/src/test/java/org/apache/niolex/commons/internal/FinallyTest.java
@@ -18,12 +18,19 @@
 package org.apache.niolex.commons.internal;
 
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import org.apache.niolex.commons.concurrent.ThreadUtil;
+import org.apache.niolex.commons.reflect.MethodUtil;
+import org.apache.niolex.commons.util.Runner;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -32,6 +39,9 @@ import org.junit.Test;
  * @since 2013-6-26
  */
 public class FinallyTest extends Finally {
+
+    private int readCnt = 0;
+    private int writeCnt = 0;
 
     @Test(expected=IOException.class)
     public void testWriteAndClose() throws Exception {
@@ -45,6 +55,57 @@ public class FinallyTest extends Finally {
         InputStream in = mock(InputStream.class);
         doThrow(new IOException("Mock")).when(in).read(any(byte[].class));
         transferAndClose(in, mock(OutputStream.class), 1024);
+    }
+
+    public void readTest(int k) {
+        readCnt += k;
+        ThreadUtil.sleep(50);
+    }
+
+    public void writeTest(int k) {
+        writeCnt += k;
+        ThreadUtil.sleep(50);
+    }
+
+    private Method r = MethodUtil.getMethods(getClass(), "readTest")[0];
+    private Method w = MethodUtil.getMethods(getClass(), "writeTest")[0];
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public void letsRead(int k) throws Throwable {
+        useReadLock(lock, this, r, new Object[] {k});
+    }
+
+    public void letsWrite(int k) throws Throwable {
+        useWriteLock(lock, this, w, new Object[] {k});
+    }
+
+    @Before
+    public void before() {
+        readCnt = 0;
+        writeCnt = 0;
+    }
+
+    @Test
+    public void testUseReadLock() throws Exception {
+        Runner.run(this, "letsRead", 3);
+        Runner.run(this, "letsRead", 4);
+        ThreadUtil.sleep(5);
+        Runner.run(this, "letsWrite", 5);
+        ThreadUtil.sleep(5);
+        assertEquals(7, readCnt);
+        assertEquals(0, writeCnt);
+    }
+
+    @Test
+    public void testUseWriteLock() throws Exception {
+        Runner.run(this, "letsWrite", 2);
+        ThreadUtil.sleep(5);
+        Runner.run(this, "letsRead", 3);
+        Runner.run(this, "letsRead", 4);
+        Runner.run(this, "letsWrite", 5);
+        ThreadUtil.sleep(5);
+        assertEquals(0, readCnt);
+        assertEquals(2, writeCnt);
     }
 
 }
