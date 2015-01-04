b96e5cf554fe153bac30dbfc4db044dd1c62a47a
Lex <xiejiyun@foxmail.com>
2015-01-04 13:47:07 +0800

    add some test for package access rules.

---
+++ commons-demo/src/main/java/org/apache/niolex/common/grammar/Hide.java
@@ -0,0 +1,50 @@
+/**
+ * Hide.java
+ *
+ * Copyright 2014 the original author or authors.
+ *
+ * We licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.niolex.common.grammar;
+
+/**
+ * @author <a href="mailto:xiejiyun@foxmail.com">Xie, Jiyun</a>
+ * @version 1.0.0
+ * @since 2014-12-10
+ */
+public class Hide {
+
+    public static class Base {
+        void give() {
+            System.out.println("Base: No ~");
+        }
+
+        void give(int i) {
+            System.out.println("Base: give you $" + i + " dollars.");
+        }
+    }
+
+    public static class CNY extends Base {
+        void give(int i) {
+            System.out.println("CNY: give you ￥" + i + " yuan.");
+        }
+    }
+
+    public static void main(String args[]) {
+        Base b = new CNY();
+
+        b.give();
+        b.give(5);
+    }
+
+}
