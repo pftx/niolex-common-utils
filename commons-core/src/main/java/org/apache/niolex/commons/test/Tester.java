3b0bb942bd514cfac843a9657663e45ea6b64565
Lex <xiejiyun@foxmail.com>
2016-03-14 17:39:35 +0800

    Add new class Tester for float and double equality test.

---
+++ commons-core/src/main/java/org/apache/niolex/commons/test/Tester.java
@@ -0,0 +1,93 @@
+/**
+ * Tester.java
+ *
+ * Copyright 2016 the original author or authors.
+ *
+ * We licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.niolex.commons.test;
+
+/**
+ * The tester to help check the value equals and between the range.
+ *
+ * @author <a href="mailto:xiejiyun@foxmail.com">Xie, Jiyun</a>
+ * @version 1.0.0
+ * @since 2016-3-10
+ */
+public class Tester {
+
+    /**
+     * The float system inaccuracy.
+     * The float accuracy: 6 -7 bit.
+     */
+    public static final double FLOAT_INACCURACY = 1.0e-6f;
+
+    /**
+     * The double system inaccuracy.
+     * The double accuracy: 15 - 16 bit.
+     */
+    public static final double DOUBLE_INACCURACY = 1.0e-15;
+
+    /**
+     * Test whether two integers equals each other.
+     *
+     * @param a
+     * @param b
+     * @return true if equal, false if not
+     */
+    public static final boolean equal(int a, int b) {
+        return a == b;
+    }
+
+    /**
+     * Test whether two float equals each other.
+     *
+     * @param a
+     * @param b
+     * @return true if equal, false if not
+     */
+    public static final boolean equal(float a, float b) {
+        return between(-FLOAT_INACCURACY, (a - b), FLOAT_INACCURACY);
+    }
+
+    /**
+     * Test whether two double equals each other.
+     *
+     * @param a
+     * @param b
+     * @return true if equal, false if not
+     */
+    public static final boolean equal(double a, double b) {
+        return between(-DOUBLE_INACCURACY, (a - b), DOUBLE_INACCURACY);
+    }
+
+    /**
+     * Test whether the value b set between a and c.
+     */
+    public static final boolean between(int a, int b, int c) {
+        return a <= b && b <= c;
+    }
+
+    /**
+     * Test whether the value b set between a and c.
+     *
+     * @param a
+     * @param b
+     * @param c
+     * @return true if between, false if not
+     */
+    public static final boolean between(double a, double b, double c) {
+        return a <= b && b <= c;
+    }
+
+}
---
+++ commons-core/src/test/java/org/apache/niolex/commons/test/TesterTest.java
@@ -0,0 +1,58 @@
+package org.apache.niolex.commons.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesterTest extends Tester {
+
+    @Test
+    public void testEqualIntInt() throws Exception {
+        assertTrue(equal(3, 3));
+        assertFalse(equal(3, 2));
+    }
+
+    @Test
+    public void testEqualFloat() throws Exception {
+        assertTrue(equal(3.0f, 3.0f));
+        assertTrue(equal(3.0f,  3.0000001f));
+        assertTrue(equal(3.0f,  3.0000002f));
+        assertTrue(equal(3.0f,  3.0000003f));
+        assertFalse(equal(3.0f, 3.000002f));
+    }
+
+    @Test
+    public void testEqualDoubleDouble() throws Exception {
+        assertTrue(equal(3.0, 3.0));
+        assertTrue(equal(3.0,  3.000000000000001));
+        assertTrue(equal(3.0,  3.0000000000000002));
+        assertTrue(equal(3.0,  3.0000000000000003));
+        assertFalse(equal(3.0, 3.000000000000002));
+    }
+
+
+    @Test
+    public void testEqualDD() throws Exception {
+        assertTrue(equal(1/3.0, 2/6.0));
+        assertTrue(equal(1/10.0, 0.1));
+    }
+
+    @Test
+    public void testBetweenIntIntInt() throws Exception {
+        assertTrue(between(4, 4, 4));
+        assertTrue(between(4, 4, 5));
+        assertTrue(between(3, 4, 4));
+        assertFalse(between(5, 4, 4));
+        assertFalse(between(4, 4, 3));
+    }
+
+    @Test
+    public void testBetweenDoubleDoubleDouble() throws Exception {
+        assertTrue(between(4.0, 4.0, 4.0));
+        assertTrue(between(4.0, 4.00000000000001, 4.000000000000011));
+        assertFalse(between(4.0, 4.00000000000001d, 4.0));
+        assertFalse(between(4.00000000000001, 4.00000000000001, 4.0));
+        assertFalse(between(4.0, 4.00000000000002, 4.00000000000001));
+    }
+
+}
