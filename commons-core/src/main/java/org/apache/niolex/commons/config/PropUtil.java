bf880b23fb1f7bce80ad0364057ee9ba3ab24223
Lex <pftxie@qq.com>
2016-12-30 18:37:41 +0800

    Remove all the chinese javadoc, add English javadocs.

--- commons-core/src/main/java/org/apache/niolex/commons/config/PropUtil.java
+++ commons-core/src/main/java/org/apache/niolex/commons/config/PropUtil.java
@@ -21,54 +21,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
- * PropUtil是一个用来管理properties配置文件的工具类
- * <pre>
- * 目前提供的功能如下：
- * 1-1. public static final void loadConfig(String resource, Class&lt;T&gt; cls)
- * 从class path中加载指定名字的配置文件
- *
- * 1-2. public static final void loadConfig(String fileName)
- * 从文件系统中加载指定名字的配置文件
- *
- * 2-1. public static final String getProperty(String key)
- * 从配置文件中读取字符串类型的配置
- *
- * 2-2. public static final String getProperty(String key, String defaultValue)
- * 从配置文件中读取字符串类型的配置
- *
- * 3-1. public static final String getString(String key)
- * 从配置文件中读取字符串类型的配置
- *
- * 3-2. public static final String getString(String key, String defaultValue)
- * 从配置文件中读取字符串类型的配置
- *
- * 4-1. public static final int getInteger(String key)
- * 从配置文件中读取整数类型的配置
- *
- * 4-2. public static final int getInteger(String key, String defaultValue)
- * 从配置文件中读取整数类型的配置
- *
- * 4-3. public static final int getInteger(String key, int defaultValue)
- * 从配置文件中读取整数类型的配置
- *
- * 5-1. public static final long getLong(String key)
- * 从配置文件中读取长整数类型的配置
- *
- * 5-2. public static final long getLong(String key, String defaultValue)
- * 从配置文件中读取长整数类型的配置
- *
- * 5-3. public static final long getLong(String key, long defaultValue)
- * 从配置文件中读取长整数类型的配置
- *
- * 6-1. public static boolean getBoolean(String key)
- * 从配置文件中读取布尔类型的配置
- *
- * 6-2. public static boolean getBoolean(String key, String defaultValue)
- * 从配置文件中读取布尔类型的配置
- *
- * 6-3. public static boolean getBoolean(String key, boolean defaultValue)
- * 从配置文件中读取布尔类型的配置
- * </pre>
+ * The PropUtil can manage a list of properties files, the one loaded later will override the one loaded earlier. Users
+ * can retrieve property as int, long, boolean, double and String from it.
+ * 
  * @author <a href="mailto:xiejiyun@gmail.com">Xie, Jiyun</a>
  * @version 1.0.0
  */
@@ -77,179 +32,234 @@ public abstract class PropUtil {
     private static final PropertiesWrapper PROPS = new PropertiesWrapper();
 
     /**
-     * 从class path中加载指定名字的配置文件
+     * Load properties from the specified classpath resource file using the same
+     * class loader which loaded the specified class.
      *
-     * @param resource 待加载的文件的资源名
-     * @param cls 携带待使用的类加载器
-     * @throws IOException 假如读文件出错
-     * @throws IllegalArgumentException 假如文件中含有非法字符
-     * @throws NullPointerException 假如文件名是null
+     * @param resource the classpath resource file
+     * @param cls the class object to be used to retrieve class loader
+     * @throws IOException if failed to read the resource file
+     * @throws IllegalArgumentException if the file contains illegal character
+     * @throws NullPointerException if any of the parameters is null
      */
     public static final void loadConfig(String resource, Class<?> cls) throws IOException {
         PROPS.load(cls.getResourceAsStream(resource));
     }
 
     /**
-     * 从文件系统中加载指定名字的配置文件
+     * Load properties from the specified file. The <code>fileName</code> must be an
+     * absolute file path and name in the file system.
      *
-     * @param fileName 待加载的文件名
-     * @throws IOException 假如读文件出错
-     * @throws IllegalArgumentException 假如文件中含有非法字符
-     * @throws NullPointerException 假如文件名是null
-     * @throws SecurityException 假如系统中配置了security manager并且该security manager拒绝了读文件的请求
+     * @param fileName the absolute file path and name
+     * @throws IOException if failed to read the resource file
+     * @throws IllegalArgumentException if the file contains illegal character
+     * @throws NullPointerException if any of the parameters is null
+     * @throws SecurityException the configured security manager rejected the requirement to read the specified file
      */
     public static final void loadConfig(String fileName) throws IOException {
         PROPS.load(new FileInputStream(fileName));
     }
 
     /**
-     * 从配置文件中读取字符串类型的配置，不对字符串进行trim
+     * Searches for the property with the specified key in this property list.
+     * If the key is not found in this property list, the default property list,
+     * and its defaults, recursively, are then checked. The method returns
+     * <code>null</code> if the property is not found.
      *
-     * @param key 待读取的配置的键
-     * @return 待读取的配置的值，如果配置不存在则返回null
+     * @param key the property key.
+     * @return the value in this property list with the specified key value.
      */
     public static final String getProperty(String key) {
         return PROPS.getProperty(key);
     }
 
     /**
-     * 从配置文件中读取字符串类型的配置，不对字符串进行trim
+     * Searches for the property with the specified key in this property list.
+     * If the key is not found in this property list, the default property list,
+     * and its defaults, recursively, are then checked. The method returns the
+     * default value argument if the property is not found.
      *
-     * @param key 待读取的配置的键
-     * @param defaultValue 待读取的配置的默认值
-     * @return 待读取的配置的值，如果配置不存在则返回<code>defaultValue</code>
+     * @param key the hashtable key.
+     * @param defaultValue a default value.
+     * @return the value in this property list with the specified key value.
      */
     public static final String getProperty(String key, String defaultValue) {
         return PROPS.getProperty(key, defaultValue);
     }
 
     /**
-     * 从配置文件中读取字符串类型的配置并对字符串进行trim
+     * Searches for a string value from properties by the specified key. We will trim
+     * the empty characters before return. We will return null if the property not found.
      *
      * @see #getProperty(String)
-     * @param key 待读取的配置的键
-     * @return 待读取的配置的值，如果配置不存在则返回null
+     * @param key the property key to be used to retrieve property
+     * @return the property value, or null if the property not found
      */
     public static final String getString(String key) {
         return PROPS.getString(key);
     }
 
     /**
-     * 从配置文件中读取字符串类型的配置并对字符串进行trim
+     * Searches for a string value from properties by the specified key. We will trim the empty characters before
+     * return. We will return the specified default value if the property not found.
      *
      * @see #getProperty(String, String)
-     * @param key 待读取的配置的键
-     * @param defaultValue 待读取的配置的默认值
-     * @return 待读取的配置的值，如果配置不存在则返回<code>defaultValue</code>
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value
+     * @return the property value, or the <code>defaultValue</code> if the property not found
      */
     public static final String getString(String key, String defaultValue) {
         return PROPS.getString(key, defaultValue);
     }
 
     /**
-     * 从配置文件中读取整数类型的配置
+     * Searches for an integer value from properties by the specified key.
      *
-     * @param key 待读取的配置的键
-     * @return 待读取的配置的值，如果配置不存在则抛出NumberFormatException
-     * @throws NumberFormatException 如果配置不存在,或者配置不是可以解析的整数
+     * @param key the property key to be used to retrieve property
+     * @return the property value as integer
+     * @throws NumberFormatException if the property not found or is not a valid integer
      */
     public static final int getInteger(String key) {
         return PROPS.getInteger(key);
     }
 
     /**
-     * 从配置文件中读取整数类型的配置
+     * Read an integer value from properties by the specified key. If the property not found, we will use the specified
+     * default value.
      *
-     * @param key 待读取的配置的键
-     * @param defaultValue 待读取的配置的默认值，请确保该默认值是可以解析的整数
-     * @return 待读取的配置的值，如果配置不存在则使用<code>defaultValue</code>
-     * @throws NumberFormatException 如果配置不存在时<code>defaultValue</code>不是可以解析的整数，
-     *         或者配置不是可以解析的整数
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value, please make sure it's a valid integer
+     * @return the property value as integer, or the <code>defaultValue</code> if the property not found
+     * @throws NumberFormatException if the property not found or is not a valid integer or the specified
+     *             <code>defaultValue</code> is not a valid integer
      */
     public static final int getInteger(String key, String defaultValue) {
         return PROPS.getInteger(key, defaultValue);
     }
 
     /**
-     * 从配置文件中读取整数类型的配置
+     * Read an integer value from properties by the specified key. If the property not found, we will use the specified
+     * default value.
      *
-     * @param key 待读取的配置的键
-     * @param defaultValue 待读取的配置的默认值
-     * @return 待读取的配置的值，如果配置不存在则使用<code>defaultValue</code>
-     * @throws NumberFormatException 如果配置不是可以解析的整数
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value
+     * @return the property value as integer, or the <code>defaultValue</code> if the property not found
+     * @throws NumberFormatException if the property not found or is not a valid integer
      */
     public static final int getInteger(String key, int defaultValue) {
         return PROPS.getInteger(key, defaultValue);
     }
 
     /**
-     * 从配置文件中读取长整数类型的配置
+     * Read a long integer value from properties by the specified key.
      *
-     * @param key 待读取的配置的键
-     * @return 待读取的配置的值，如果配置不存在则抛出NumberFormatException
-     * @throws NumberFormatException 如果配置不存在,或者配置不是可以解析的整数
+     * @param key the property key to be used to retrieve property
+     * @return the property value as long integer
+     * @throws NumberFormatException if the property not found or is not a valid long integer
      */
     public static final long getLong(String key) {
     	return PROPS.getLong(key);
     }
 
     /**
-     * 从配置文件中读取长整数类型的配置
+     * Read a long integer value from properties by the specified key. If the property not found, we will use the
+     * specified default value.
      *
-     * @param key 待读取的配置的键
-     * @param defaultValue 待读取的配置的默认值，请确保该默认值是可以解析的整数
-     * @return 待读取的配置的值，如果配置不存在则使用<code>defaultValue</code>
-     * @throws NumberFormatException 如果配置不存在时<code>defaultValue</code>不是可以解析的整数，
-     *         或者配置不是可以解析的整数
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value, please make sure it's a valid long integer
+     * @return the property value as long integer, or the <code>defaultValue</code> if the property not found
+     * @throws NumberFormatException if the property not found or is not a valid long integer or the specified
+     *             <code>defaultValue</code> is not a valid long integer
      */
     public static final long getLong(String key, String defaultValue) {
     	return PROPS.getLong(key, defaultValue);
     }
 
     /**
-     * 从配置文件中读取长整数类型的配置
+     * Read a long integer value from properties by the specified key. If the property not found, we will use the
+     * specified default value.
      *
-     * @param key 待读取的配置的键
-     * @param defaultValue 待读取的配置的默认值
-     * @return 待读取的配置的值，如果配置不存在则使用<code>defaultValue</code>
-     * @throws NumberFormatException 如果配置不是可以解析的整数
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value
+     * @return the property value as long integer, or the <code>defaultValue</code> if the property not found
+     * @throws NumberFormatException if the property not found or is not a valid long integer
      */
     public static final long getLong(String key, long defaultValue) {
     	return PROPS.getLong(key, defaultValue);
     }
 
     /**
-     * 从配置文件中读取布尔类型的配置
-     * 当且仅当配置为字符串"true", "1", "on", "yes"(忽略大小写)时，返回true,其他情况一概返回false
+     * Read boolean value from properties by the specified key. We will
+     * interpret "true", "1", "on", "yes"(case insensitive) as true, all other values as false.
      *
-     * @param key 待读取的配置的键
-     * @return 待读取的配置的值，（请注意）如果配置不存在则返回false
+     * @param key the property key to be used to retrieve property
+     * @return the property value, or false if the property not found
      */
     public static boolean getBoolean(String key) {
         return PROPS.getBoolean(key);
     }
 
     /**
-     * 从配置文件中读取布尔类型的配置
-     * 当且仅当配置为字符串"true", "1", "on", "yes"(忽略大小写)时，返回true,其他情况一概返回false
+     * Read boolean value from properties by the specified key. We will
+     * interpret "true", "1", "on", "yes"(case insensitive) as true, all other values as false.
      *
-     * @param key 待读取的配置的键
-     * @param defaultValue 待读取的配置的默认值
-     * @return 待读取的配置的值，如果配置不存在则使用<code>defaultValue</code>
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value
+     * @return the property value as boolean, or the <code>defaultValue</code> if the property not found
      */
     public static boolean getBoolean(String key, String defaultValue) {
         return PROPS.getBoolean(key, defaultValue);
     }
 
     /**
-     * 从配置文件中读取布尔类型的配置
-     * 当且仅当配置为字符串"true", "1", "on", "yes"(忽略大小写)时，返回true,其他情况一概返回false
+     * Read boolean value from properties by the specified key. We will
+     * interpret "true", "1", "on", "yes"(case insensitive) as true, all other values as false.
      *
-     * @param key 待读取的配置的键
-     * @param defaultValue 待读取的配置的默认值
-     * @return 待读取的配置的值，如果配置不存在则使用<code>defaultValue</code>
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value
+     * @return the property value as boolean, or the <code>defaultValue</code> if the property not found
      */
     public static boolean getBoolean(String key, boolean defaultValue) {
         return PROPS.getBoolean(key, defaultValue);
     }
+
+    /**
+     * Read a double value from properties by the specified key.
+     * 
+     * @param key the property key to be used to retrieve property
+     * @return the property value as double
+     * @throws NullPointerException if the property not found
+     * @throws NumberFormatException if the property is not a valid double number
+     */
+    public double getDouble(String key) {
+        return PROPS.getDouble(key);
+    }
+
+    /**
+     * Read a double value from properties by the specified key. If the property not found, we will use the specified
+     * default value.
+     * 
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value
+     * @return the property value as double, or the <code>defaultValue</code> if the property not found
+     * @throws NullPointerException if the property not found and the <code>defaultValue</code> is null
+     * @throws NumberFormatException if the property is not a valid double number or the default value is not a valid
+     *             double number
+     */
+    public double getDouble(String key, String defaultValue) {
+        return PROPS.getDouble(key, defaultValue);
+    }
+
+    /**
+     * Read a double value from properties by the specified key. If the property not found, we will use the specified
+     * default value.
+     * 
+     * @param key the property key to be used to retrieve property
+     * @param defaultValue the default value
+     * @return the property value as double, or the <code>defaultValue</code> if the property not found
+     * @throws NumberFormatException if the property is not a valid double number
+     */
+    public double getDouble(String key, double defaultValue) {
+        return PROPS.getDouble(key, defaultValue);
+    }
+
 }
